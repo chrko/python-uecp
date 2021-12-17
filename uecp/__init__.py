f020c3d70927aec5507d877817473811d90bd716
Christian Kohlstedde <christian@kohlsted.de>
2021-12-17 23:20:41 +0100

    Release 0.2.0

--- uecp/__init__.py
+++ uecp/__init__.py
@@ -1,6 +1,6 @@
 """UECP encoder & decoder"""
 
-__version__ = "0.1.0a1"
+__version__ = "0.2.0"
 
 # import custom codecs to ensure registering search functions
 from uecp import byte_stuffing_codec, rds_character_set_codec
