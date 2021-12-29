8f82ac3311c82939688b095c5c546e6337f7075a
Christian Kohlstedde <christian@kohlsted.de>
2021-12-29 23:16:02 +0100

    Bump version

--- uecp/__init__.py
+++ uecp/__init__.py
@@ -1,6 +1,6 @@
 """UECP encoder & decoder"""
 
-__version__ = "0.3.0"
+__version__ = "0.4.0a1"
 
 # import custom codecs to ensure registering search functions
 from uecp import byte_stuffing_codec, rds_character_set_codec
