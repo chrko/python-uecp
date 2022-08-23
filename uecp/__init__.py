829f6019640cf86cb6ba43a1b2b0672e8d57ea1f
Christian Kohlstedde <christian@kohlsted.de>
2022-08-23 09:34:13 +0200

    Fix static type check error

--- uecp/__init__.py
+++ uecp/__init__.py
@@ -5,4 +5,4 @@ __version__ = "0.4.0a1"
 # import custom codecs to ensure registering search functions
 from uecp import byte_stuffing_codec, rds_character_set_codec  # noqa: F401
 
-__all__ = []
+__all__: list[str] = []
