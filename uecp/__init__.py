8402c5051745d5269140c9dd90810a1f74b4ff61
Christian Kohlstedde <christian@kohlsted.de>
2022-08-23 09:22:32 +0200

    Fix unused import errors

--- uecp/__init__.py
+++ uecp/__init__.py
@@ -3,4 +3,6 @@
 __version__ = "0.4.0a1"
 
 # import custom codecs to ensure registering search functions
-from uecp import byte_stuffing_codec, rds_character_set_codec
+from uecp import byte_stuffing_codec, rds_character_set_codec  # noqa: F401
+
+__all__ = []
