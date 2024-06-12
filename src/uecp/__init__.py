cb8d0af5e345c8facd8e4fceb943a391ca9d8ad5
Christian Kohlstedde <christian@kohlsted.de>
2024-06-13 00:25:38 +0200

    Release version

--- src/uecp/__init__.py
+++ src/uecp/__init__.py
@@ -1,6 +1,6 @@
 """UECP encoder & decoder"""
 
-__version__ = "0.4.0a1"
+__version__ = "0.4.0"
 
 # import custom codecs to ensure registering search functions
 from uecp import byte_stuffing_codec, rds_character_set_codec  # noqa: F401
